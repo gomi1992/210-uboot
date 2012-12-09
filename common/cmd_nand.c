8234b32a1ed15f9b8c8da8bd24ceead7d976a76f
Alex Ling <kasimling@gmail.com>
2012-12-09 10:51:37 +0800

    Fix build for mini210 and tiny210
    Signed-off-by: Alex Ling <kasimling@gmail.com>

--- common/cmd_nand.c
+++ common/cmd_nand.c
@@ -671,6 +671,9 @@ static int do_nand(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 				printf("Unknown nand command suffix '%s'.\n", s);
 				return 1;
 			}
+#ifndef WITH_INLINE_OOB
+#define WITH_INLINE_OOB WITH_YAFFS_OOB
+#endif
 			ret = nand_write_skip_bad(nand, off, &rwsize,
 						(u_char *)addr,
 						WITH_INLINE_OOB);
